3ceddbd1baecdecd8d8c00c33dd4580137919244
Isidor <isschoch@ethz.ch>
2020-06-30 16:31:44 +0200

    Implemented Euclidean Metric with basic testing passed

--- my_programs/hmc_sampling/_model/model.py
+++ my_programs/hmc_sampling/_model/model.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
-
+import numpy as np
 
 # This is the negative square -(x^2)
 def model(s):
-  v1 = s["Parameters"][0]
-  v2 = s["Parameters"][1]
-  v3 = s["Parameters"][2]
-  s["P(x)"] = -0.5 * (v1 * v1 + v2 * v2 + v3 * v3)
+  # v1 = s["Parameters"][0]
+  # v2 = s["Parameters"][1]
+  
+  v = np.array(s["Parameters"])
+  # print(np.shape(v))
+  dim = len(v)
+  # cov = np.array([[1.0, 0.0], [0.0, 1.0]], dtype=float)
+  cov = np.identity(dim, dtype=float)
+  cov_inv = np.linalg.inv(cov)
+
+  mu = np.zeros(dim, dtype=float)
+  v_centred = v - mu
+  # print(np.shape(covariance))
+  # s["P(x)"] = -0.5 * (v1 * v1)
+  s["P(x)"] = -0.5 * np.matmul(np.matmul(v_centred.T, cov_inv), v_centred)
